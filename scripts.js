9eadf17ba5d3a4fc0d8038b13aefd45698bc5e53
OpenShift guest <ebjc@aol.com>
2013-12-11 22:35:30 -0500

    final almost

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ the first 5 entities in the users collection that contain the property status:'a
 var dataClient = new Apigee.Client({
     orgName:'cattiee', //your Apigee organization name
     appName:'cattiee'
-});    		
+});        	
 			
 var options = {
 	endpoint:"items", //the collection to query
@@ -21,137 +21,154 @@ dataClient.request(options, function (error, response) {
 	}	
 });
 */
-
-
 //write error handling here
-function completeTask(task){
-    if (typeof task.type == 'undefined'){
+
+/*function completeTask(task) {
+ if (typeof task.name == 'undefined'){
+        fireQ(newItemName);
         throw ('"Item is undefined" + items[i]');
+    
+var newItemAisle;
+var newItemName;
+if (task.newItem === 'undefined'){ 
+                grabItem(newItemName, newItemAisle);
+            }
+            
+} */          
+
+var items;
+    
+//ajax request for item  //only fired on submit
+function promptTest(newItemName){
+    
+    var newItemAisle = prompt('Item not found. Enter an Aisle number.');
+    if($.isNumeric(newItemAisle)) {
+        addItem(newItemName, newItemAisle);
+    } else {
+        return false;
     }
+    //else grabItem(newItemName, newItemAisle);
 }
-
-
-$(document).ready(function() { 
-/*
-            1. Set your account details in the app
-
-            - Enter your orgName below — it’s the username you picked when you signed up at apigee.com
-            - Keep the appName as “sandbox”: it’s a context we automatically created for you. 
-            It’s completely open by default, but don’t worry, other apps you create are not!      */
-    var client = new Apigee.Client({
-        orgName: 'cattiee',
-        appName: 'sandbox'
-    });
-/*
-            2. Set some details for your first object
-
-            Great, we know where your where account is now!
-            Let’s try to create a book, save it on Apigee, and output it on the page.
-            
-            - Keep the type as “book”.
-            - Enter the title of your favorite book below, instead of “the old man and the sea”.    */
-    var items = {
-        type: 'items',
-        name: 'oranges',
-        aisle: 1
-    };
-/*
-            3. Now, run it!
-            
-            You’re good to go! Open index.html in a web browser to see it running!
-            
-            - This app will NOT work in Internet Explorer due to their lack of CORS support.
-            - Please use Firefox, Chrome or Safari, etc. instead.      */
-    client.createEntity(items, function(error, response) {
-        if (error) { // Error - the item was not saved properly
-            alert("Could not create the item. Did you enter your orgName (username) correctly on line 18 of index.html?");
-        } else { // Success - the book was created properly
-            // The saved object is returned in the “response” variable,
-            // defined on line 46. The code below outputs it on the page!
-            document.getElementsByTagName('body')[0].innerHTML += "Success! Here is the object we stored; " + "notice the timestamps and unique id we created for you:" + "<br/><br/>" + JSON.stringify(response.get());
-        }
-/*
-            4. Congrats, you’re done!
-
-            - You can try adding more properties after line 34 and reloading the page!
-            - You can then see the admin view of this data by logging in at https://apigee.com/usergrid
-            - Or you can go explore more advanced examples in our docs: http://apigee.com/docs/usergrid         */
-        
-        //end Apigee directions
-        
-
-  
-       
-        
-        var items = [{
-            type: 'items',
-            name: 'Oranges',
-            aisle: 4
-        }];
-        
-        var itemsList = '<table class = "table">';
-        for (var i = 0; i < items.length; i++) {
-          $('.table').append('<tr>' + '<td>' + items[i].name + '</td>' + '<td>' + items[i].aisle + '</td>' + '</tr>');
-            
-            try {
-            itemsList += completeTask(items[i]);
-        } catch (error) {
-            console.log("There was an error:" + error);
-        }
-            
-            itemsList += '</table>';
-            console.log(itemsList);
-            $('.panel panel-default').append(itemsList);
-        }
-
-       
-// function append item to list form
-        $('#submitItemTable').on('submit', function() {
-            event.preventDefault();
-            var submitItem = $(this).find('input').val();
-            addItem(submitItem);
-            console.log(submitItem);
-        });
-
-/*
-function append items to database list
-if item is found return item name and aisle number // call function append to list form
-else   items not found alert 'would you like to add this item to the database' 
-'this requires the item name and aisle number' 
-*/
-
-
-//Object Constructor Function
-
-        function Item(description) {
-            var obj = {};
-            obj.name = name;
-            obj.aisle = 1;
-            return obj;
-        }
-
-        function addItem(description) {
-            var newItem = new Item();
-            items.push(newItem);
-            client.createEntity.push(newItem);
-            
-            
-            //  ???  append to Apigee  query database for item 
- /*           if (queryDatabase === true){
-                $('.panel panel-default').append(itemsList);
-                console.log(newItem);
-            else {
-                return ("Item is not found, would you like to add this item to the database?");
+function grabItem(newItemName, newItemAisle) {
+        $.ajax({
+            'url': "https://api.usergrid.com/cattiee/sandbox/items?ql=select * where name = '"+ newItemName +"'",
+            //pass newItemName to api ?ql
+            'type': 'GET',
+            'success': function(data) {
+                if(data.entities.length > 0) {
+                items = data.entities;
+                    var itemsList = $('<tr>'); // + '<td>' + items[i].name + '</td>' + '<td>' + items[i].aisle + '</td>';
+                    for (var i = 0; i < items.length; i++) {
+                         // console.log(items[i]);// first call to console
+                        itemsList += '<tr>' + '<td>' + items[i].name + '</td>' + '<td>' + items[i].aisle + '</td>';
+                        $('.table').append(itemsList);// append itemsList to the table
+                    }
+                } else {
+                    promptTest(newItemName);
+                    //
+                    //var newItemAisle = prompt('Item not found. Enter an Aisle number.');
+                    //if prompt cancel 
+                    //if(newItemAisle != "" && newItemAisle !== null) {
+                    /* */
+                    //addItem(newItemName, newItemAisle);
                 }
-            } */
-         }
+            }
+        });
+            //append to list
+}        
+            
 
-       
-        
-        
-        
-//function to sort aisle content in ascending or descending order
-        
-        //close document.ready
+    function Item(newItemName, newItemAisle) { // I added a second parameter to the argument here
+        var obj = {};
+        obj.name = newItemName;
+        obj.aisle = newItemAisle; // || 0; // this means - if newItemAisle is null, default to zero
+        return obj;
+    }
+    //function to add Item to API and append to list
+
+    function addItem(newItemName, newItemAisle) {
+        this.name = newItemName;
+        this.aisle = newItemAisle;
+        var newItem = new Item(newItemName, newItemAisle);
+        //console.log(JSON.stringify(newItem));
+        $.ajax({
+            'url': 'https://api.usergrid.com/cattiee/sandbox/items',
+            'type': 'POST',
+            'data': JSON.stringify(newItem),
+            'success': function(data) {
+                console.log(data);
+               var newItemHtml = '<tr>' + '<td>' + newItem.name + '</td>' + '<td>' + newItem.aisle + '</td>'; 
+                 
+            newItemHtml += "</tr>";
+            $('.table').append(newItemHtml);// add the name and aisle number to table
+            } 
+        });
+       /* items.push(newItem);
+        console.log(newItem);
+        $('.table').append(newItem);*/
+    }
+    
+    
+
+$(document).ready(function(){  
+    $.ajax({
+        'url': 'https://api.usergrid.com/cattiee/sandbox/items',
+        'type': 'GET',
+        'success': function(data) {
+            console.log(data.entities);
+            items = data.entities;
+            var itemsList = $('<tr>'); // + '<td>' + items[i].name + '</td>' + '<td>' + items[i].aisle + '</td>';
+            for (var i = 0; i < items.length; i++) {
+                //   console.log(items[i]);// first call to console
+                itemsList += '<tr>' + '<td>' + items[i].name + '</td>' + '<td>' + items[i].aisle + '</td>';
+/* try {
+                itemsList += completeTask(items[i]);
+                } catch (error) {
+                    console.log("There was an error:" + error);
+                }*/
+            }
+            //console.log(itemExists);//checking for query success 
+            itemsList += '</tr>';
+            //console.log(itemsList);
+            $('.table').append(itemsList);
+        }
     });
+ 
+
+
+    //  }           
+    // $('newItemAisle').val('formula to reorder-ascending'); 
+    $('#submitItemTable').on('submit', function(event) {
+        event.preventDefault();
+        var newItemName = $(this).find('#newItemName').val();
+        var newItemAisle = $(this).find('#newItemAisle').val();
+        //completeTask(newItemName, newItemAisle);
+        grabItem(newItemName,newItemAisle);
+        //addItem(newItemName, newItemAisle);
+        console.log(newItemName, newItemAisle);
+    });
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
